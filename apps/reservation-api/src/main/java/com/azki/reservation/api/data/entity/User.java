0fe471c34df2d46859cfe174c979574a75db977c
Alireza Esmaeeli <alrz.smili@gmail.com>
2025-02-27 21:14:00 +0330

    add user specs

--- apps/reservation-api/src/main/java/com/azki/reservation/api/data/entity/User.java
+++ apps/reservation-api/src/main/java/com/azki/reservation/api/data/entity/User.java
@@ -2,6 +2,7 @@ package com.azki.reservation.api.data.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.FieldNameConstants;
 
 import java.util.Collections;
 import java.util.HashSet;
@@ -13,6 +14,7 @@ import java.util.Set;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(callSuper = true)
 @Getter
+@FieldNameConstants
 public class User extends BaseEntity {
 
     @Column(unique = true, nullable = false)
---
+++ apps/reservation-api/src/main/java/com/azki/reservation/api/data/spec/UserSpecs.java
@@ -0,0 +1,10 @@
+package com.azki.reservation.api.data.spec;
+
+import com.azki.reservation.api.data.entity.User;
+import org.springframework.data.jpa.domain.Specification;
+
+public abstract class UserSpecs {
+    public static Specification<User> hasEmail(String email) {
+        return (root, query, cb) -> cb.equal(root.get(User.Fields.email), email);
+    }
+}
