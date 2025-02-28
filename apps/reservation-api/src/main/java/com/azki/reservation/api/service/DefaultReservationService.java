97aeac5800c0cd813965aeaa3d42016d4f5d1e17
Alireza Esmaeeli <alrz.smili@gmail.com>
2025-02-28 16:41:07 +0330

    pass test with maximum locking

--- apps/reservation-api/src/main/java/com/azki/reservation/api/service/DefaultReservationService.java
+++ apps/reservation-api/src/main/java/com/azki/reservation/api/service/DefaultReservationService.java
@@ -8,26 +8,37 @@ import com.azki.reservation.api.exception.AvailableSlotNotFoundException;
 import com.azki.reservation.api.exception.ReservationNotFoundException;
 import com.azki.reservation.api.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class DefaultReservationService implements ReservationService {
 
     private final ReservationRepository reservationRepository;
     private final UserRepository userRepository;
     private final AvailableSlotRepository availableSlotRepository;
 
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     @Override
-    public Reservation reserveFirstAvailableSlot(String userEmail) {
+    public synchronized Reservation reserveFirstAvailableSlot(String userEmail) {
         var user = userRepository.findByEmail(userEmail)
                 .orElseThrow(UserNotFoundException::new);
+        log.debug("Found user {}", user.getId());
 
         var availableSlot = availableSlotRepository.findFirstNotReserved()
                 .orElseThrow(AvailableSlotNotFoundException::new);
+        log.debug("Found available slot {}", availableSlot.getId());
 
         var reservation = new Reservation(availableSlot, user);
-        reservationRepository.save(reservation);
+        log.debug("Created reservation of available slot {} for user {}", availableSlot.getId(), user.getId());
+
+        reservationRepository.saveAndFlush(reservation);
+        log.debug("Saved reservation {}", reservation.getId());
+
         return reservation;
     }
 
