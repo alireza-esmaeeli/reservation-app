bf564a893f308bd3e3c964dc355e8fa401d2060b
Alireza Esmaeeli <alrz.smili@gmail.com>
2025-02-28 03:42:19 +0330

    add swagger

--- apps/reservation-api/src/main/java/com/azki/reservation/api/controller/ReservationController.java
+++ apps/reservation-api/src/main/java/com/azki/reservation/api/controller/ReservationController.java
@@ -4,10 +4,8 @@ import com.azki.reservation.api.dto.ReservationCreateDTO;
 import com.azki.reservation.api.dto.ReservationQueryDTO;
 import com.azki.reservation.api.service.ReservationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
 
 @RestController
 @RequestMapping("reservations")
@@ -16,22 +14,21 @@ public class ReservationController {
 
     private final ReservationService reservationService;
 
-    @PostMapping
-    public ResponseEntity<ReservationQueryDTO> createReservation(@RequestBody ReservationCreateDTO dto) {
+    @PostMapping(produces = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ReservationQueryDTO createReservation(@RequestBody ReservationCreateDTO dto) {
         var reservation = reservationService.reserveFirstAvailableSlot(dto.userEmail());
-        return ResponseEntity
-                .created(URI.create("/reservations/" + reservation.getId()).normalize())
-                .body(new ReservationQueryDTO(
-                        reservation.getId(),
-                        reservation.getUser().getEmail(),
-                        reservation.getAvailableSlot().getStartTime(),
-                        reservation.getAvailableSlot().getEndTime())
-                );
+        return new ReservationQueryDTO(
+                reservation.getId(),
+                reservation.getUser().getEmail(),
+                reservation.getAvailableSlot().getStartTime(),
+                reservation.getAvailableSlot().getEndTime()
+        );
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteReservation(@PathVariable Long id) {
         reservationService.cancelReservation(id);
-        return ResponseEntity.noContent().build();
     }
 }
